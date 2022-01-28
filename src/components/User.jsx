5586c608033ff8f0e592c5859d1322568709f3c9
shakoanony <mamniashvilishako@gmailcom>
2022-01-28 21:45:09 +0400

    built statick page

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 
 function User() {
   return (
-   
+    <a href="https://www.github.com" target="_blank">
       <div className="Container">
         <img src="/twitter.png" alt="user"  /> 
         <div className="Content">
           <h3>Name</h3>
         </div>
       </div>
+    </a>
   )
 }
 
